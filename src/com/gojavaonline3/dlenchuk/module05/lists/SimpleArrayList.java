1c12016116e0cc6649b8a66c796cc5d62ccfc5e5
Dmitrij0 <dmitrijlenchuk@gmail.com>
2016-06-06 15:35:31 +0300

    Branch has been created

--- src/com/gojavaonline3/dlenchuk/module05/lists/SimpleArrayList.java
+++ src/com/gojavaonline3/dlenchuk/module05/lists/SimpleArrayList.java
@@ -18,46 +18,6 @@ public class SimpleArrayList<T extends Number & Comparable> implements SimpleLis
         checkList();
     }
 
-    public enum SortKind {
-
-        BUBBLE(new Sortable<Comparable>() {
-            @Override
-            public void sort(Comparable[] list) {
-                for (int i = 1; i < list.length; i++) {
-                    for (int j = i; j > 0; j--) {
-                        if (list[j].compareTo(list[j - 1]) < 0) {
-                            Comparable buffer = list[j];
-                            list[j] = list[j - 1];
-                            list[j - 1] = buffer;
-                        }
-                    }
-                }
-            }
-        }),
-        MERGE(new Sortable<Comparable>() {
-            @Override
-            public void sort(Comparable[] list) {
-                // ToDo;
-            }
-        }),
-        DO_NOT_SORT(new Sortable<Comparable>() {
-            @Override
-            public void sort(Comparable[] list) {
-                // NOP;
-            }
-        });
-
-        private final Sortable sort;
-
-        SortKind(Sortable sort) {
-            this.sort = sort;
-        }
-
-        public Sortable getSort() {
-            return sort;
-        }
-    }
-
     private Sortable sort = BUBBLE.getSort();
 
     @Override
